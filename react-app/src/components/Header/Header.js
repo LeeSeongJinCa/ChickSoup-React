5f158805cbb2a564f0f51af833c910862444fc9a
이성진 <chlskatnr1@naver.com>
2020-01-20 20:15:27 +0900

    [CREATE] <Header> header 파일 생성
    2020.01.20

---
+++ react-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './Styled';
+
+const Header = () => {
+    return (
+        <Styled.Header>
+            
+        </Styled.Header>
+    )
+};
+
+export default Header;
