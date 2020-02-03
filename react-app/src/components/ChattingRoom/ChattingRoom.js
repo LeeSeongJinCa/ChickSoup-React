ae925ce241660d10987a984db744f16713726fa3
이성진 <chlskatnr1@naver.com>
2020-02-03 17:01:29 +0900

    [ADD] <ChattingRoom> 채팅방 리스트 구현
    2020.02.03

--- react-app/src/components/ChattingRoom/ChattingRoom.js
+++ react-app/src/components/ChattingRoom/ChattingRoom.js
@@ -2,6 +2,62 @@ import React, { useEffect } from 'react';
 import * as Styled from './Styled';
 import SearchBox from '../FriendList/SearchBox';
 import ListNav from '../FriendList/ListNav';
+import newChat from '../img/newChatting.svg';
+
+const roomData = [
+    {
+        rooms: [10, 13, 14, 16],
+        title: "DMS1",
+        people: 4,
+        hurry: true,
+    }, {
+        rooms: [10, 13, 14],
+        title: "DMS2",
+        people: 3,
+        hurry: false,
+    }, {
+        rooms: [10, 13],
+        title: "DMS3",
+        people: 2,
+        hurry: true,
+    }, {
+        rooms: [10],
+        title: "DMS4",
+        people: 1,
+        hurry: false,
+    }, {
+        rooms: [10],
+        title: "DMS4",
+        people: 1,
+        hurry: true,
+    }, {
+        rooms: [10],
+        title: "DMS4",
+        people: 1,
+        hurry: false,
+    }, {
+        rooms: [10],
+        title: "DMS4",
+        people: 1,
+        hurry: true,
+    }, 
+];
+
+const makeRooms = () => {
+    const rooms = roomData.map((room, i) => {
+        return (
+            <li key={i}>
+                <div>
+                    <img src={`http://chicksoup.s3.ap-northeast-2.amazonaws.com/media/image/user/profile/${room.rooms[0]}.png`} alt="dms" />
+                    {room.hurry && <span></span>}
+                </div>
+                <h2>{room.title}</h2>
+                <p>{room.people}</p>
+            </li>
+        )
+    });
+    return rooms;
+};
 
 const ChattingRoom = ({ onLoadAuth }) => {
     useEffect(() => {
@@ -12,7 +68,13 @@ const ChattingRoom = ({ onLoadAuth }) => {
             <SearchBox title={"채팅하기"} placeholder={"채팅방 검색"} />
             <ListNav />
             <section>
-                
+                <ul>
+                    {makeRooms()}
+                    <li className="new-chat">
+                        <img src={newChat} alt="newChat" />
+                        <p>새 채팅</p>
+                    </li>
+                </ul>
             </section>
         </Styled.ChattingRoom>
     )
--- react-app/src/components/ChattingRoom/Styled.js
+++ react-app/src/components/ChattingRoom/Styled.js
@@ -3,6 +3,59 @@ import styled from 'styled-components';
 const ChattingRoom = styled.main`
     width: 1220px;
     margin: 0 auto;
+    > section {
+        > ul {
+            display: flex;
+            flex-wrap: wrap;
+            align-items: center;
+            > li {
+                width: 160px;
+                margin: 8px 42px 24px;
+                text-align: center;
+                transition: 0.3s ease-in-out;
+                cursor: pointer;
+                > div {
+                    position: relative;
+                    display: flex;
+                    justify-content: center;
+                    width: 160px;
+                    height: 160px;
+                    border: 1px solid #1A1A1A;
+                    border-radius: 16px;
+                    > img {
+                        align-self: center;
+                        max-width: 160px;
+                        max-height: 160px;
+                        border-radius: 16px;
+                    }
+                    > span {
+                        position: absolute;
+                        right: -10px;
+                        bottom: -10px;
+                        width: 24px;
+                        height: 24px;
+                        border-radius: 50%;
+                        background-color: #FEC200
+                    }
+                }
+                > h2 {
+                    font-size: 28px;
+                    margin-bottom: 8px;
+                    padding: 8px 0;
+                    border-bottom: 1px solid #1E1E1E;
+                }
+                > p { font-size: 16px; }
+            }
+            > li.new-chat > p {
+                font-size: 20px;
+                font-weight: bold;
+            }
+            > li:hover {
+                border-radius: 16px;
+                transform: scale(1.05);
+            }
+        }
+    }
 `;
 
 export { ChattingRoom };
