e8c0a7f5aa7630f48bba00d5e4bb6ff8dd884f48
이성진 <chlskatnr1@naver.com>
2020-07-16 14:34:23 +0900

    asd

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './components/2grade/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
